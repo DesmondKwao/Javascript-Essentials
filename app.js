7a9a9f928f0fb946dad3443b354c4a6f25eaa33a
DesmondKwao <72413189+DesmondKwao@users.noreply.github.com>
2020-10-12 01:50:48 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 let selection = document.getElementsByName('desgin');
-
 let getId = document.getElementById('main');
-
 function set()
 {
-
     for( let entry = 0; entry < selection.length; entry++ )  /* all selection are saved in an array */
     {
         let desgin = selection[ entry ].getAttribute('id');
-
         let setSelect = selection[ entry ].value;
-
-        getId.style[ desgin ] = setSelect;
+       getId.style[ desgin ] = setSelect;
     }
 }
-
 document.getElementById('set').addEventListener('click',set); /* dispaly the id and values selected from the arrary */
